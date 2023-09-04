6cd061d6d771b7635326c340632d39f7295610d0
songyunjeong <yungunaa@gmail.com>
2023-09-05 00:07:35 +0900

    LinkedList

---
+++ LinkedList.mjs
@@ -0,0 +1,119 @@
+/*
+  추상자료형
+  : 데이터와 그 데이터를 연산하는 기능을 표기
+
+  연결리스트의 추상자료형
+  1. 모든 데이터 출력  printAll()
+  2. 모든 데이터 삭제  clear()
+  3. 인덱스 삽입  insertAt(index, data)
+  4. 마지막 삽입  insertLast(data)
+  5. 인덱스 삭제  deleteAt(index)
+  6. 마지막 삭제  deleteLast()
+  7. 인덱스 읽기  getNodeAt(index)
+*/
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null; // 시작 노드
+    this.count = 0; // 총 저장된 노드 수
+  }
+
+  printAll() {
+    let currentNode = this.head;
+    let text = "[";
+
+    while (currentNode != null) {
+      text += currentNode.data;
+      currentNode = currentNode.next;
+
+      if (currentNode != null) {
+        text += ", ";
+      }
+    }
+
+    text += "]";
+    console.log(text);
+  }
+
+  clear() {
+    this.head = null;
+    this.count = 0;
+  }
+
+  insertAt(index, data) {
+    if (index > this.count || index < 0) {
+      throw new Error("범위를 넘어갔습니다.");
+    }
+
+    let newNode = new Node(data); // 새로 삽입할 노드
+
+    if (index == 0) {
+      // 가장 앞부분 삽입
+      newNode.next = this.head;
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+      newNode.next = currentNode.next;
+      currentNode.next = newNode;
+    }
+    this.count++;
+  }
+
+  insertLast(data) {
+    this.insertAt(this.count, data);
+  }
+
+  deleteAt(index) {
+    if (index >= this.count || index < 0) {
+      throw new Error("제거할 수 없습니다.");
+    }
+
+    let currentNode = this.head;
+
+    if (index == 0) {
+      let deletedNode = this.head;
+      this.head = this.head.next;
+      this.count--;
+      return deletedNode;
+    } else {
+      for (let i = 0; i < index - 1; i++) {
+        currentNode = currentNode.next;
+      }
+
+      let deletedNode = currentNode.next;
+      currentNode.next = currentNode.next.next;
+      this.count--;
+      return deletedNode;
+    }
+  }
+
+  deleteLast() {
+    return this.deleteAt(this.count - 1);
+  }
+
+  getNodeAt(index) {
+    if (index >= this.count || index < 0) {
+      throw new Error("범위를 넘어갔습니다.");
+    }
+
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next;
+    }
+
+    return currentNode;
+  }
+}
+
+export { Node, LinkedList };
